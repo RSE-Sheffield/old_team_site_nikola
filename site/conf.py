32e0798196a03b1252f216c20dec5f4ecfbad1fd
Mike Croucher <Michael.P.Croucher@googlemail.com>
2015-12-19 00:52:34 +0000

    Added site URL

--- site/conf.py
+++ site/conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Mike Croucher"  # (translatable)
 BLOG_TITLE = "Research Software Engineering at Sheffield"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "http://rse-sheffield.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
